2a3e5cc89bbf5790bb14180e03bca3fa15333588
Steve <stevejryan@gmail.com>
2017-02-20 13:12:46 -0500

    Adding previously processed XLSX file for testing

--- randomDendriteLocations.py
+++ randomDendriteLocations.py
@@ -42,16 +42,34 @@ def main():
     dend_workbook = xlrd.open_workbook(filename)
     first_sheet = dend_workbook.sheet_by_index(0)
     segment_column = first_sheet.col_values(1)
+    
+    if first_sheet.ncols > 2:
+        fourth_column = first_sheet.col_values(3)
+        fifth_column = first_sheet.col_values(4)
+        # third_column and fourth_column don't have obviously cleaner names
+        # they refer to the columns added to the spreadsheet by the previous v
+        # version of this function.  They store the locations selected by that 
+        # script.  For example, next to a segment of length 294 which has been 
+        # selected may be, in column_three, the numeral 1098.  The Thousands place
+        # is superfluous, and the 98 indicates 98 microns from the proximal point
+        # of origin for this segment.  We'll need to convert these to numerical
+        # values that make sense in the schema used here.
+    
+    running_total_column = []
+    running_total = 0
+    for i in segment_column:
+        running_total += i
+        running_total_column.append(running_total)
 
     existing_list = [12, 150, 400, 2009] # toy list, just for testing
     number_existing = len(existing_list) # establish number of pre-existing points
     number_to_randomize = 10 - number_existing # hardcodes 10 segments as desired output
 
-    updated_list = randomLocation(existing_list, number_to_randomize, segment_column)
+    updated_list = randomLocation(existing_list, number_to_randomize, segment_column, running_total_column)
     print(updated_list)
 
 
-def randomLocation(existing_list, number_of_locations, dendrite_segment_list):
+def randomLocation(existing_list, number_of_locations, dendrite_segment_list, running_total_column):
     # Input - existing_list
     #           List of random points already selected that are being kept, if
     #           any.  This can be empty.  
@@ -69,12 +87,6 @@ def randomLocation(existing_list, number_of_locations, dendrite_segment_list):
     if number_to_generate <= 0:
         return new_list
 
-    running_total_column = []
-    running_total = 0
-    for i in dendrite_segment_list:
-        running_total += i
-        running_total_column.append(running_total)
-
     total_length = running_total_column[len(running_total_column)-1]
 
     for i in range(number_to_generate):
@@ -84,7 +96,7 @@ def randomLocation(existing_list, number_of_locations, dendrite_segment_list):
             # test validity, must be more than 50 um from existing points
             # repeat or break based on outcome of validity test
             new_location = random.randint(1, math.floor(total_length))
-            new_list_array = numpy.array(new_list)
+            new_list_array = array(new_list)
             new_list_array = abs(new_list_array - new_location)
             if min(new_list_array) < 50:
                 print(new_list)
@@ -99,6 +111,13 @@ def randomLocation(existing_list, number_of_locations, dendrite_segment_list):
         new_list.append(new_location)
 
     return new_list
+
+
+def convertOldList(segment_column, third_column, fourth_column):
+    # stuff
+    return 0
+    
+    
 
 # 
 if __name__ == "__main__":
